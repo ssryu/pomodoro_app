45a5b16c9294db655dc82ec7b11d808f49137a37
ssryu <jjkztok3@gmail.com>
2023-01-15 20:59:42 +0900

    Add pause method

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,10 +22,21 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void onStartPressed() {
     timer = Timer.periodic(
-        const Duration(
-          seconds: 1,
-        ),
-        onTick);
+      const Duration(
+        seconds: 1,
+      ),
+      onTick,
+    );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -51,8 +62,10 @@ class _HomeScreenState extends State<HomeScreen> {
           flex: 3,
           child: Center(
             child: IconButton(
-              onPressed: onStartPressed,
-              icon: const Icon(Icons.play_circle_outline),
+              onPressed: isRunning ? onPausePressed : onStartPressed,
+              icon: Icon(isRunning
+                  ? Icons.pause_circle_outline
+                  : Icons.play_circle_outline),
               iconSize: 120,
               color: Theme.of(context).cardColor,
             ),
